e9158f0a6328920ce7844987c96c8582feb5ea57
aburahan8721 <aburaihan8721@gmail.com>
2023-05-11 10:37:13 +0600

    navbar problem solved

--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -6,11 +6,11 @@ const Header = () => {
     <header className="header_area">
       <nav className="navbar navbar-expand-lg">
         <div className="container">
-          <a className="navbar-brand" href="#">
-            Navbar
+          <a className="navbar_brand" href="#">
+            TalentTrack
           </a>
           <button
-            className="navbar-toggler"
+            className="navbar-toggler navbar_toggler_btn"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent"
@@ -18,18 +18,28 @@ const Header = () => {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span className="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon toggler_icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="#">
+              <li className="nav_item">
+                <a className="nav_link" aria-current="page" href="#">
                   Home
                 </a>
               </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
+              <li className="nav_item">
+                <a className="nav_link" href="#">
+                Statistics
+                </a>
+              </li>
+              <li className="nav_item">
+                <a className="nav_link" href="#">
+                Applied Jobs
+                </a>
+              </li>
+              <li className="nav_item">
+                <a className="nav_link" href="#">
+                Blog
                 </a>
               </li>
             </ul>
